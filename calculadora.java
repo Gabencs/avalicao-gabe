2c7eb713be8fce75526bd5276e46db2fee5d6eae
Gabriel <gabedropac@gmail.com>
2026-05-11 15:34:09 -0300

    Adiciona função de Subtracao

---
+++ calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+
+    int resultado = soma(10, 5);
+      System.out.println("Resultado da soma: " + resultado);
+    }
+}
