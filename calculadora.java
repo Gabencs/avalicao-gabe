518afc272d4dafbd2e5d6b5db5f8e784dc3e0b42
Gabriel <gabedropac@gmail.com>
2026-05-11 15:43:24 -0300

    feat:Adiciou a subtracao

--- calculadora.java
+++ calculadora.java
@@ -1,11 +1,19 @@
 public class Calculadora {
-    
+
     public static int soma(int a, int b) {
         return a + b;
     }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
 
-    int resultado = soma(10, 5);
-      System.out.println("Resultado da soma: " + resultado);
+        int resultadoSoma = soma(10, 5);
+        int resultadoSubtracao = subtracao(10, 5);
+
+        System.out.println("Resultado da soma: " + resultadoSoma);
+        System.out.println("Resultado da subtração: " + resultadoSubtracao);
     }
 }
